9d4b15724fab3f45fe9df42ced10c688211d5b5b
taanhvu <ex-anhvu001@ocft.com>
2022-09-18 02:13:37 +0700

    update

--- server/index.js
+++ server/index.js
@@ -21,12 +21,48 @@ const socketIO = require('socket.io')(http, {
 });
 
 let users = [];
+let messages = {"groups": []};
 
 socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
 
     socket.on('message', (data) => {
-        socketIO.emit('messageResponse', data);
+        if (data.receiverId) {
+            if(typeof messages[socket.id + data.receiverId] === 'undefined'){
+                messages[socket.id + data.receiverId] = [];
+            }
+            messages[socket.id + data.receiverId].push(data);
+
+            if(typeof messages[data.receiverId + socket.id] === 'undefined'){
+                messages[data.receiverId + socket.id] = [];
+            }
+            messages[data.receiverId + socket.id].push(data);
+
+            socketIO.to(socket.id).emit('messageResponse', data);
+            let receiver = users.filter(user => user.socketID === data.receiverId)[0];
+            if(receiver.currentReceiver === socket.id){
+                socketIO.to(data.receiverId).emit('messageResponse', data);
+            }
+        } else {
+            messages.groups.push(data);
+            let canReceiveUsers = users.filter(user => user.currentReceiver === null).map(data => data.socketID);
+            socketIO.to(canReceiveUsers).emit('messageResponse', data);
+        }
+    });
+
+    socket.on('changeReceiver', (data) => {
+        let currentUser = users.filter(user => user.socketID === socket.id)[0];
+        if (data.receiverId) {
+            if(currentUser){
+                currentUser['currentReceiver'] = data.receiverId;
+            }
+            socketIO.to(socket.id).emit('changeReceiverResponse', messages[data.receiverId + socket.id] ? messages[data.receiverId + socket.id] : []);
+        } else {
+            if(currentUser){
+                currentUser['currentReceiver'] = null;
+            }
+            socketIO.to(socket.id).emit('changeReceiverResponse', messages.groups);
+        }
     });
 
     //Listens when a new user joins the server
